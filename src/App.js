b44322f473664f4e7c411d641bcce4f747bc170e
scottskinn <84105761+scottskinn@users.noreply.github.com>
2022-12-07 15:59:25 -0500

    FPV + movement

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Sky } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import { Ground } from './components/Ground'
 import { Player } from './components/Player'
+import { FPV } from './components/FPV'
 
 
 function App() {
@@ -11,11 +12,13 @@ function App() {
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
         <ambientLight intensity={0.5}/>
+          <FPV />
         <Physics>
           <Player />
           <Ground />
         </Physics>
       </Canvas>
+      <div className='absolute center cursor'>+</div>
     </>
   );
 }
